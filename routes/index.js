deb8e443339f64f3a632cb8ae08c793a2daa11e2
Sebastián Tamashiro <sebastian@alotama.com>
2019-12-10 20:08:49 -0300

    Enable cors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const graphqlHTTP = require('express-graphql')
 const schemas = require('../schemas/championSchema')
+const cors = require('cors')
 const root = require('../services')
 
+const corsOptions = {
+  "origin": process.env.WHITELIST,
+  "methods": "GET",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
 const init = (server) => {
-  server.use('/graphql', graphqlHTTP({
+  server.use('/graphql', cors(corsOptions), graphqlHTTP({
     schema: schemas,
     rootValue: root,
     graphiql: true
